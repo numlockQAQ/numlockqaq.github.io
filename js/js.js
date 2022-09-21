4f9716f1cc9b3cbebdbf893809e18cacb9983718
numlockQAQ <614150457@qq.com>
2022-09-22 00:08:55 +0800

    03

---
+++ js/js.js
@@ -0,0 +1,13 @@
+<script>
+const background = document.querySelector(".background")
+
+        document.addEventListener('scroll', () => {
+            const scrollY = window.scrollY
+
+            if (scrollY !== 0) {
+                background.style.backgroundPosition = `calc(50% + ${scrollY}px) calc(50% + ${scrollY}px)`
+            }else{
+                background.style.backgroundPosition = ''
+            }
+        })
+</script>
